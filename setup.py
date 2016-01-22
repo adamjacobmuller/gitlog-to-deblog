842e6be78f7bfef4cae782c3edf481d2d3aec6d6
Adam Jacob Muller <adam@isprime.com>
2016-01-21 20:20:13 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitlog-to-deblog',
-    version='1.0.0',
+    version='1.0.1',
     author='Adam Jacob Muller',
     packages=find_packages(),
     author_email='adam@isprime.com',
